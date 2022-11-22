156b118013fcec9b169459c927968b00e43c07be
Wetxius <Wetxius@users.noreply.github.com>
2022-11-22 13:47:48 +0300

    Adjusted positions for RaidCD (move slighlty to right), stance and pet bar (anchor to action bar on top left), raid tanks if enabled threat.

--- QulightUI/Core/Panels.lua
+++ QulightUI/Core/Panels.lua
@@ -54,8 +54,8 @@ end
 ----------------------------------------------------------------------------------------
 local PetBarAnchor = CreateFrame("Frame", "PetActionBarAnchor", T_PetBattleFrameHider)
 if C.actionbar.petbar_horizontal == true then
-	PetBarAnchor:CreatePanel("Invisible", (C.actionbar.button_size * 10) + (C.actionbar.button_space * 9), (C.actionbar.button_size + C.actionbar.button_space), unpack(C.position.pet_horizontal))
-elseif C.actionbar.rightbars > 0 or C.actionbar.rightbars == false then
+	PetBarAnchor:CreatePanel("Invisible", (C.actionbar.button_size * 10) + (C.actionbar.button_space * 9), C.actionbar.button_size, unpack(C.position.pet_horizontal))
+elseif C.actionbar.rightbars > 0 then
 	PetBarAnchor:CreatePanel("Invisible", C.actionbar.button_size + C.actionbar.button_space, (C.actionbar.button_size * 10) + (C.actionbar.button_space * 9), "RIGHT", RightBarAnchor, "LEFT", 0, 0)
 else
 	PetBarAnchor:CreatePanel("Invisible", (C.actionbar.button_size + C.actionbar.button_space), (C.actionbar.button_size * 10) + (C.actionbar.button_space * 9), unpack(C.position.right_bars))
