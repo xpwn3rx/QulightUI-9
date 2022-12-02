62030381483767407e1fa1d23261b6941d64b72f
Wetxius <Wetxius@users.noreply.github.com>
2022-12-02 20:15:16 +0300

    [RaidBuffsReminder] Added "Blessing of the Bronze" buff and more phials.

--- QulightUI/Modules/Auras/RaidBuffsReminder.lua
+++ QulightUI/Modules/Auras/RaidBuffsReminder.lua
@@ -10,8 +10,9 @@ local guardianelixirbuffs = T.ReminderBuffs["GuardianElixir"]
 local foodbuffs = T.ReminderBuffs["Food"]
 local staminabuffs = T.ReminderBuffs["Stamina"]
 local versbuffs = T.ReminderBuffs["Vers"]
+local reducebuffs = T.ReminderBuffs["Reduce"]
 local custombuffs = T.ReminderBuffs["Custom"]
-local visible, flask, battleelixir, guardianelixir, food, stamina, vers, spell4, reduce, custom, weapon
+local visible, flask, battleelixir, guardianelixir, food, stamina, vers, spell4, reduce, custom, weapon, armor
 local playerBuff = {}
 local icons = {}
 local UpdatePositions
@@ -176,6 +177,21 @@ local function OnAuraChange(_, event, arg1)
 		end
 	end
 
+	for i = 1, #reducebuffs do
+		local name, icon = unpack(reducebuffs[i])
+		if i == 1 then
+			ReduceFrame.t:SetTexture(icon)
+		end
+		if playerBuff[name] then
+			ReduceFrame:SetAlpha(C.reminder.raid_buffs_alpha)
+			reduce = true
+			break
+		else
+			ReduceFrame:SetAlpha(1)
+			reduce = false
+		end
+	end
+
 	if #custombuffs > 0 then
 		for i = 1, #custombuffs do
 			local name, icon = unpack(custombuffs[i])
@@ -201,7 +217,7 @@ local function OnAuraChange(_, event, arg1)
 	if (not IsInGroup() or instanceType ~= "raid") and C.reminder.raid_buffs_always == false then
 		RaidBuffReminder:SetAlpha(0)
 		visible = false
-	elseif flask == true and food == true and stamina == true and spell4 == true and custom == true and weapon == true and vers == true and reduce == true then
+	elseif flask == true and food == true and stamina == true and spell4 == true and custom == true and weapon == true and armor == true and vers == true and reduce == true then
 		if not visible then
 			RaidBuffReminder:SetAlpha(0)
 			visible = false
@@ -243,6 +259,7 @@ local buffButtons = {
 	"StaminaFrame",
 	"VersFrame",
 	"Spell4Frame",
+	"ReduceFrame",
 	"CustomFrame",
 }
 
