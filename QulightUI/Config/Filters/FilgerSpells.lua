cf7233a74e788ba8899d7e435f975aa44a73cbba
Wetxius <Wetxius@users.noreply.github.com>
2022-12-11 11:22:59 +0300

    [FilgerSpells] Added class table to start populate trinkets procs.

--- QulightUI/Config/Filters/FilgerSpells.lua
+++ QulightUI/Config/Filters/FilgerSpells.lua
@@ -3253,50 +3253,38 @@ do
 	tinsert(T.CustomFilgerSpell, {"COOLDOWN", {slotID = 13, filter = "CD"}})
 	tinsert(T.CustomFilgerSpell, {"COOLDOWN", {slotID = 14, filter = "CD"}})
 
-	local isTank = {["DEATHKNIGHT"] = true, ["DEMONHUNTER"] = true, ["DRUID"] = true, ["MONK"] = true, ["PALADIN"] = true, ["WARRIOR"] = true}
-	local isHealer = {["DRUID"] = true, ["EVOKER"] = true, ["MONK"] = true, ["PALADIN"] = true, ["PRIEST"] = true, ["SHAMAN"] = true}
-	local strengthClass = {["DEATHKNIGHT"] = true, ["PALADIN"] = true, ["WARRIOR"] = true}
-	local agilityClass = {["DEMONHUNTER"] = true, ["DRUID"] = true, ["HUNTER"] = true, ["MONK"] = true, ["ROGUE"] = true, ["SHAMAN"] = true}
-	local intellectClass = {["DRUID"] = true, ["EVOKER"] = true, ["MAGE"] = true, ["MONK"] = true, ["PALADIN"] = true, ["PRIEST"] = true, ["SHAMAN"] = true, ["WARLOCK"] = true}
+	local strengthClass = {
+		["DEATHKNIGHT"] = true,
+		["PALADIN"] = true,
+		["WARRIOR"] = true,
+	}
+
+	local agilityClass = {
+		["DEMONHUNTER"] = true,
+		["DRUID"] = true,
+		["HUNTER"] = true,
+		["MONK"] = true,
+		["ROGUE"] = true,
+		["SHAMAN"] = true,
+	}
+
+	local intellectClass = {
+		["DRUID"] = true,
+		["EVOKER"] = true,
+		["MAGE"] = true,
+		["MONK"] = true,
+		["PALADIN"] = true,
+		["PRIEST"] = true,
+		["SHAMAN"] = true,
+		["WARLOCK"] = true,
+	}
 
 	-- Trinkets
+	-- Strength classes
 	if strengthClass[T.class] then
 		-- Bound by Fire and Blaze [Blazebinder's Hoof]
 		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 383926, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-		-- Bonemaw's Big Toe
+		-- Bonemaw's Big Toe [Bonemaw's Big Toe]
 		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 397400, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
 	end
-
-	if agilityClass[T.class] then
-		-- Bottle of Spiraling Winds
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 383751, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-		-- Windswept Pages
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 126483, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-	end
-
-	if agilityClass[T.class] or strengthClass[T.class] then
-		-- Scent of Blood [Hunger of the Pack]
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 213888, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-		-- Algeth'ar Puzzle
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 383781, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-	end
-
-	if intellectClass[T.class] then
-		-- Power Theft
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 382126, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-	end
-
-	if isHealer[T.class] then
-		-- Broodkeeper's Promise
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 377462, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-		-- Voidmender's Shadowgem
-		tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 397399, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-	end
-
-	-- Crumbling Power [Irideus Fragment]
-	tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 383941, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-	-- Whispering Incarnate Icon
-	tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 377452, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
-	-- Valarjar's Path [Horn of Valor]
-	tinsert(T.CustomFilgerSpell, {"P_PROC_ICON", {spellID = 215956, unitID = "player", caster = "all", filter = "BUFF", absID = true}})
 end
