80f96945a5e1d4e4d2d2df2c6cd14189f30ab49c
Wetxius <Wetxius@users.noreply.github.com>
2022-12-18 20:03:34 +0300

    [Skins] Use another function to skin loot buttons.

--- QulightUI/Modules/Skins/Blizzard/Loot.lua
+++ QulightUI/Modules/Skins/Blizzard/Loot.lua
@@ -119,11 +119,6 @@ local function LoadSkin()
 		if button.QualityStripe then
 			button.QualityStripe:SetAlpha(0)
 		end
-		if button.IconQuestTexture:IsShown() then
-			item:SetBackdropBorderColor(1, 1, 0)
-		else
-			item:SetBackdropBorderColor(unpack(C.media.border_color))
-		end
 	end)
 end
 
