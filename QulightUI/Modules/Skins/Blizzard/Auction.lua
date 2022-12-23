535404c9b8ba0e55b8b9938c577b7816ad4c3eab
Wetxius <Wetxius@users.noreply.github.com>
2022-12-23 11:46:39 +0300

    [Skins] Fixed Auctionator skin.

--- QulightUI/Modules/Skins/Blizzard/Auction.lua
+++ QulightUI/Modules/Skins/Blizzard/Auction.lua
@@ -423,11 +423,6 @@ local function LoadSkin()
 				end
 			end
 		end)
-		local frame = CreateFrame("Frame")
-		frame:RegisterEvent("TRADE_SKILL_SHOW")
-		frame:SetScript("OnEvent", function(_, event)
-			AuctionatorCraftingInfoProfessionsFrame.SearchButton:SkinButton()
-		end)
 	end
 end
 
