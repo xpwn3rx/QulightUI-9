f07dd5b0d445b032e43daf308c79cd42566d14e2
Frode <frode.h@aktivportal.com>
2023-01-01 23:49:14 +0100

    Added more Evoker Preservation spells to Cooldowns

--- QulightUI/Config/Filters/FilgerSpells.lua
+++ QulightUI/Config/Filters/FilgerSpells.lua
@@ -728,7 +728,7 @@ C["filger_spells"] = {
 			-- Chrono Loop (PvP Talent)
 			{spellID = 383005, filter = "CD"},
 			-- Emerald Communion
-			{spellID = 370960, filter = "CD"},
+			{spellID = 370960, filter = "CD"},	
 			-- Dream Breath
 			{spellID = 355941, filter = "CD"},
 			-- Spiritbloom
