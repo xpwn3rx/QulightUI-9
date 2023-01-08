057fafa303360a509b0407708bfa9991d57511f6
Wetxius <Wetxius@users.noreply.github.com>
2023-01-08 19:15:55 +0300

    [MiniMap] Fixed mail frame for 10.0.5.

--- QulightUI/Config/Filters/FilgerSpells.lua
+++ QulightUI/Config/Filters/FilgerSpells.lua
@@ -626,7 +626,7 @@ C["filger_spells"] = {
 			-- Buffs
 			-- Snapfire
 			{spellID = 370818, unitID = "player", caster = "player", filter = "BUFF"},
-			-- Essence burst
+			-- Essence Burst
 			{spellID = 359618, unitID = "player", caster = "player", filter = "BUFF"},
 			-- Leaping Flames
 			{spellID = 370901, unitID = "player", caster = "player", filter = "BUFF"},
