fa4c5fd7e53362c08183e8a5e0b8490feae9fd4d
Wetxius <Wetxius@users.noreply.github.com>
2023-04-15 11:12:40 +0300

    Hide QuestScrollFrame.Edge in 10.1.0.

--- QulightUI/Modules/Skins/Blizzard/WorldMap.lua
+++ QulightUI/Modules/Skins/Blizzard/WorldMap.lua
@@ -41,7 +41,9 @@ local function LoadSkin()
 	QuestScrollFrame.DetailFrame.BottomDetail:Hide()
 	QuestScrollFrame.Contents.Separator.Divider:Hide()
 	QuestScrollFrame:SetSize(259, 463)
-	QuestScrollFrame.Edge:Hide()
+	if T.newPatch then
+		QuestScrollFrame.Edge:Hide()
+	end
 
 	local CampaignOverview = QuestMapFrame.CampaignOverview
 	CampaignOverview:StripTextures()
