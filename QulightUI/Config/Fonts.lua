Shestak/ShestakUI

[GUI] Added profiles for settings. Export and import available. Moves are now independent for the character when this option is enabled.

ShestakUI/Config/Fonts.lua
@@ -117,7 +117,22 @@ C["font"] = {
 ----------------------------------------------------------------------------------------
 --	Pixel font replacement
 ----------------------------------------------------------------------------------------
-if (ShestakUIOptions and ShestakUIOptions["font"] and ShestakUIOptions["font"]["global_font"] == true) or (ShestakUIOptionsPerChar and ShestakUIOptionsPerChar["font"] and ShestakUIOptionsPerChar["font"]["global_font"] == true) then
+local disablePixel
+if ShestakUIOptionsGlobal then
+	if ShestakUIOptionsGlobal[T.realm] and ShestakUIOptionsGlobal[T.realm][T.name] and ShestakUIOptionsGlobal[T.realm]["Current_Profile"] then
+		local i = tostring(ShestakUIOptionsGlobal[T.realm]["Current_Profile"][T.name])
+		if ShestakUIOptionsPerChar and ShestakUIOptionsPerChar[i] and ShestakUIOptionsPerChar[i]["font"] and ShestakUIOptionsPerChar[i]["font"]["global_font"] then
+			disablePixel = true
+		end
+	else
+		local i = tostring(ShestakUIOptionsGlobal["Current_Profile"])
+		if ShestakUIOptions and ShestakUIOptions[i] and ShestakUIOptions[i]["font"] and ShestakUIOptions[i]["font"]["global_font"] then
+			disablePixel = true
+		end
+	end
+end
+
+if disablePixel then
 	C["media"].pixel_font = C.media.normal_font
 	C["media"].pixel_font_size = 11
 	C["media"].pixel_font_style = "OUTLINE"

xpwn3rx/QulightUI-9

QulightUI/Config/Fonts.lua
@@ -117,7 +117,22 @@ C["font"] = {
 ----------------------------------------------------------------------------------------
 --	Pixel font replacement
 ----------------------------------------------------------------------------------------
-if (QulightUIOptions and QulightUIOptions["font"] and QulightUIOptions["font"]["global_font"] == true) or (QulightUIOptionsPerChar and QulightUIOptionsPerChar["font"] and QulightUIOptionsPerChar["font"]["global_font"] == true) then
+local disablePixel
+if QulightUIOptionsGlobal then
+	if QulightUIOptionsGlobal[T.realm] and QulightUIOptionsGlobal[T.realm][T.name] and QulightUIOptionsGlobal[T.realm]["Current_Profile"] then
+		local i = tostring(QulightUIOptionsGlobal[T.realm]["Current_Profile"][T.name])
+		if QulightUIOptionsPerChar and QulightUIOptionsPerChar[i] and QulightUIOptionsPerChar[i]["font"] and QulightUIOptionsPerChar[i]["font"]["global_font"] then
+			disablePixel = true
+		end
+	else
+		local i = tostring(QulightUIOptionsGlobal["Current_Profile"])
+		if QulightUIOptions and QulightUIOptions[i] and QulightUIOptions[i]["font"] and QulightUIOptions[i]["font"]["global_font"] then
+			disablePixel = true
+		end
+	end
+end
+
+if disablePixel then
 	C["media"].pixel_font = C.media.normal_font
 	C["media"].pixel_font_size = 11
 	C["media"].pixel_font_style = "OUTLINE"